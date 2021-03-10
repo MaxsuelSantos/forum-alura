14edab34f49feb4332b4e884feff52dca6eabfaf
Maxsuel Santos <santosmaxsuel17@gmail.com>
2021-03-10 18:15:26 -0300

    Adicionando proteção com Spring Security

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$0Y/zvuWAAzH9uDSpbwHOduo5hfferAb/WUrUj3ZVnw/X2ZXWU5Ym6');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
